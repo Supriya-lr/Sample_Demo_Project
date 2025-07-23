9538f76f03bdede5e37bc247572c3cb649ec98a0
Supriya <supriya.lr23@gmail.com>
2025-07-23 19:29:47 +0530

    First Commit

---
+++ HelloWorld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello, World!"
+echo "This is a simple script to greet the world."
+pwd date
+echo "Current directory and date displayed above."
+echo "Script execution completed."
